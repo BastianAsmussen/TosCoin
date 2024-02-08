a5d9efd411fcb4433a432a0038ee8e125a55063c
Bastian Asmussen <bastian@asmussen.tech>
2024-02-08 10:47:53 +0100

    Create `coin` module.

---
+++ src/coin/mod.rs
@@ -0,0 +1,10 @@
+#[cfg(feature = "blocks")]
+pub mod blocks;
+#[cfg(feature = "chains")]
+pub mod chains;
+#[cfg(feature = "transactions")]
+pub mod transactions;
+#[cfg(feature = "wallets")]
+pub mod wallets;
+
+pub mod errors;
